83fdd4ce0c36aa9782f1b45527c396cb17dfb955
joshua-obigo <wjdgus6163@gmail.com>
2021-03-12 10:54:00 +0900

    feat: :memo: modify readme file

---
+++ 3_day/crawling.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type fooHandler struct{}
+
+func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello Foo!!</h1>")
+}
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "localhost/bar?name=\"message!\""
+	}
+	fmt.Fprintf(w, "%s", name)
+}
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello world!!</h1>")
+}
+
+func main() {
+	http.HandleFunc("/bar", barHandler)
+	http.HandleFunc("/", homeHandler)
+	http.Handle("/foo", &fooHandler{})
+	http.ListenAndServe(":3000", nil)
+}
--- examples/greeting/greeting.go
+++ examples/greeting/greeting.go
@@ -5,6 +5,11 @@ import "fmt"
 // Hello module
 func Hello(name string) string {
 	message := fmt.Sprintf("Hi, %v. Welcome 5!", name)
+	// pointer test
+	var a int = 1
+	a = 1
+	var b *int = &a
+	println(b)  // 0xc00008fec0
+	println(*b) // 1
 	return message
-
 }
